833864d80655327a1f8261bb9f97e8754d771a40
reddishmatthew <mfoslan14@gmail.com>
2024-09-30 14:53:25 -0400

    Create preprocessing.py
    Mainly a loose setup. Will be refined to meet the criteria of our chosen dataset

---
+++ preprocessing.py
@@ -0,0 +1,79 @@
+import os
+import numpy as np
+import cv2
+
+
+def load_data(data_dir):
+    '''
+    Loads and labels image data from a directory for a dementia classification.
+
+    Input:
+        data_dir (str): The path to the root directory containing subdirectories of image files. 
+                        Each subdirectory is named according to the dementia label it represents.
+    
+    Returns:
+        tuple, containing:
+            images: A numpy array of the loaded and resized image data.
+            labels: A numpy array of integer labels corresponding to each image based on the condition from label_map.
+    '''
+    labels = []
+    images = []
+
+    # Define label mapping
+    label_map = {'no_dementia': 0, 'very_mild_dementia': 1, 'mild_dimentia': 2, 'moderate_dimentia': 3}
+
+    # Iterate over each label
+    for label, value in label_map.items():
+        path = os.path.join(data_dir, label)
+        for file in os.listdir(path):
+            img_path = os.path.join(path, file)
+            image = cv2.imread(img_path)
+            if image is not None:
+                image = cv2.resize(image, (224, 224))  # Resize image to fit model input
+                images.append(image)
+                labels.append(value)
+
+    images = np.array(images)
+    labels = np.array(labels)
+
+    return images, labels
+
+
+def preprocess_data(images, labels):
+    '''
+    Normalizes image data and prepares labels.
+
+    Parameters:
+        images (np.array): Array of image data, btained from the output of the `load_data()` function.
+        labels (np.array): Array of image integer labels, obtained from the output of the `load_data()` function.
+
+    Returns:
+        tuple, containing:
+            images: The normalized image data as a numpy array.
+            labels: A numpy array of labels corresponding to each image.
+    '''
+    # Normalize pixel values by dividing by 255. 
+    images = images.astype('float32') / 255.0
+    labels = np.array(labels)
+
+    return images, labels
+
+
+if __name__ == "__main__":
+    train_data_dir = '../data/Training' # Change for the directory needed
+    test_data_dir = '../data/Testing' # Change for the directory needed
+
+    train_images, train_labels = load_data(train_data_dir)
+    test_images, test_labels = load_data(test_data_dir)
+
+    train_images, train_labels = preprocess_data(train_images, train_labels)
+    test_images, test_labels = preprocess_data(test_images, test_labels)
+
+    # Saving of processed data
+    if not os.path.exists('../data/processed'):
+        os.makedirs('../data/processed')
+
+    np.save('../data/processed/train_images.npy', train_images)
+    np.save('../data/processed/train_labels.npy', train_labels)
+    np.save('../data/processed/test_images.npy', test_images)
+    np.save('../data/processed/test_labels.npy', test_labels)
