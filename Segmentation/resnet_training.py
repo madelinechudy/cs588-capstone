d64cded9cfee90f2224ed0463775e312bf329d93
reddishmatthew <mfoslan14@gmail.com>
2024-10-17 16:31:48 -0400

    ResNet Training v.2
    Second Update. Added more imports using the build_resnet function. Added plotting to display accuracy and loss.

--- Segmentation/resnet_training.py
+++ Segmentation/resnet_training.py
@@ -1,5 +1,8 @@
 import numpy as np
 import tensorflow as tf
+from build_resnet import build_resnet, residual_block
+import os
+import matplotlib.pyplot as plt
 
 #Load preprocessed images and labels
 X_train = np.load('../cs588-capstone/Data/Processed/train_images.npy') # Shape should be (num_samples, height, width, channels)
@@ -19,12 +22,46 @@ for images, labels in train_dataset.take(1):
     print("Images shape:", images.shape)
     print("Labels shape:", labels.shape)
 
+input_shape = (224, 224, 3)  
+resnet_model = build_resnet(input_shape, num_classes=4)
+
 #Use the dataset for training
-resnet_model.fit(train_dataset, epochs=10)
+history = resnet_model.fit(train_dataset, epochs=10, batch_size=32)
 
 #Check if the directory exists and create it if not
 if not os.path.exists('../cs588-capstone/Segmentation/Models'):
     os.makedirs('../cs588-capstone/Segmentation/Models')
 
 #Save model
-resnet_model.save('../cs588-capstone/Segmentation/Models/pretrained_resnet_model.keras')
+resnet_model.save('../cs588-capstone/Segmentation/Models/resnet_model.keras')
+
+# Preservation of training history
+history_path = '../cs588-capstone/Segmentation/Models/ResNet/resnet_history.npy'
+
+if not os.path.exists(os.path.dirname(history_path)):
+    os.makedirs(os.path.dirname(history_path))
+np.save(history_path, history.history)
+
+# Visualization Training History
+if not os.path.exists('Models/ResNet'):
+    os.makedirs('Models/ResNet')
+
+# Plot and save accuracy images
+plt.figure()
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('Models/ResNet/resnet_accuracy.png')
+plt.close()
+
+# Draw and save loss images
+plt.figure()
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('Models/ResNet/resnet_loss.png')
+plt.close()
